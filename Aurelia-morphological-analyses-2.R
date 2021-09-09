b2b7536476cc4dbbc51f829251652a008ea3337d
Jonathan W. Lawley <73448607+lawleyjw@users.noreply.github.com>
2021-09-09 22:03:59 +1000

    Update Aurelia-morphological-analyses-2.R

--- Aurelia-morphological-analyses-2.R
+++ Aurelia-morphological-analyses-2.R
@@ -4,7 +4,7 @@
 #   The importance of molecular characters when morphological variability hinders   # 
 #                   diagnosability: systematics of the moon jellyfish               #
 #                           genus Aurelia (Cnidaria: Scyphozoa)                     #
-#                                 in review @ PeerJ                                 #
+#                       PeerJ https://doi.org/10.7717/peerj.11954                   #
 #                                                                                   #
 #    Used for MDS analysis in Fig. 4 and Mantel test, including fewer specimens     #
 #                         WITHOUT estimation of missing data                        #
